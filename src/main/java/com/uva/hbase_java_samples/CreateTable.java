e2223c32788d87db17a201bc8a6b64e9f37ddb85
Uva Prakash P <uvaprakash@outlook.com>
2017-12-16 11:39:46 +0530

    Added CreateTable

---
+++ src/main/java/com/uva/hbase_java_samples/CreateTable.java
@@ -0,0 +1,45 @@
+package com.uva.hbase_java_samples;
+
+/**
+ * Author: Uva Prakash P
+ * Date: 11-Dec-2017
+ *
+ */
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+
+public class CreateTable 
+{
+	   public static void main(String[] args) throws IOException {
+		   System.out.println("Initializing HBase Create Table");
+		   
+	      // Instantiating configuration class
+		  Configuration con = HBaseConfiguration.create();
+		  Connection connection = null;
+		  connection = ConnectionFactory.createConnection(con);
+
+	      // Instantiating HbaseAdmin class
+	      HBaseAdmin admin = (HBaseAdmin) connection.getAdmin();
+
+	      // Instantiating table descriptor class
+	      HTableDescriptor tableDescriptor = new
+	      HTableDescriptor(TableName.valueOf("employee"));
+
+	      // Adding column families to table descriptor
+	      tableDescriptor.addFamily(new HColumnDescriptor("personal"));
+	      tableDescriptor.addFamily(new HColumnDescriptor("professional"));
+
+	      // Execute the table through admin
+	      admin.createTable(tableDescriptor);
+	      System.out.println("Table: employee created");
+	   }
+}
